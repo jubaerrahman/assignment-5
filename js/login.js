b759695d7287d3bf72b25e8e0b0a0f760a54e46e
Jubaer Rahman <coursesmy3@gmail.com>
2026-03-09 01:49:56 +0600

    nav

--- js/login.js
+++ js/login.js
@@ -1,18 +1,10 @@
-function login(){
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-const username = document.getElementById("username").value
-const password = document.getElementById("password").value
-
-if(username === "admin" && password === "admin123"){
-
-
-
-window.location.href = "dashboard.html"
-
-}else{
-
-alert("Invalid username or password")
-
-}
-
+  if (username === "admin" && password === "admin123") {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid username or password");
+  }
 }
